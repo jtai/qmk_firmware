5e870057908106f0df2e55a9a154cd8e6ae46246
Jon Tai <jon.tai@gmail.com>
2022-01-20 22:10:03 -0800

    Basic custom keymap

---
+++ keyboards/keebio/iris/keymaps/jtai/rules.mk
@@ -0,0 +1,6 @@
+CONSOLE_ENABLE = no         # Console for debug, unused
+NKRO_ENABLE = yes           # Enable N-Key Rollover
+ENCODER_ENABLE = no
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no       # We define our own implementation
