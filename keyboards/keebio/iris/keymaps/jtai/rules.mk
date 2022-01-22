08e85820307ad1fb6bad308a53ecdb0f4e99a40d
Jon Tai <jon.tai@gmail.com>
2022-01-22 01:20:30 -0800

    Cleanups

--- keyboards/keebio/iris/keymaps/jtai/rules.mk
+++ keyboards/keebio/iris/keymaps/jtai/rules.mk
@@ -1,8 +1,9 @@
-CONSOLE_ENABLE = no         # Console for debug, unused
-NKRO_ENABLE = yes           # Enable N-Key Rollover
-ENCODER_ENABLE = no
+CONSOLE_ENABLE = no   # Console for debug, only enable when needed
+NKRO_ENABLE = yes     # Enable N-Key Rollover
+ENCODER_ENABLE = no   # Our Iris doesn't have encoders
 
-SPACE_CADET_ENABLE = no
-GRAVE_ESC_ENABLE = no       # We define our own implementation
+SPACE_CADET_ENABLE = no   # Unused, save space
+GRAVE_ESC_ENABLE = no     # We define our own implementation
+
 COMBO_ENABLE = yes
 RGB_MATRIX_CUSTOM_USER = yes
