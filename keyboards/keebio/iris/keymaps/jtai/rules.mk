01e4d8629d3fe2d2da23da669299b4be305c1c94
Jon Tai <jon.tai@gmail.com>
2022-01-24 18:53:41 -0800

    Add a game mode that adds a space key to the left side

--- keyboards/keebio/iris/keymaps/jtai/rules.mk
+++ keyboards/keebio/iris/keymaps/jtai/rules.mk
@@ -5,6 +5,7 @@ ENCODER_ENABLE = no   # Our Iris doesn't have encoders
 SPACE_CADET_ENABLE = no   # Unused, save space
 GRAVE_ESC_ENABLE = no     # We define our own implementation
 
+TAP_DANCE_ENABLE = yes
 COMBO_ENABLE = yes
 RGB_MATRIX_CUSTOM_USER = yes
 
