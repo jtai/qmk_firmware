18b34ce3a72a400aaa606c22ed674274bec50fe9
Jon Tai <jon.tai@gmail.com>
2022-02-05 20:04:59 -0800

    Set more visible default colors
    Instead of a dim white, set purple/blue at full brightness

--- keyboards/nk65/keymaps/jtai/config.h
+++ keyboards/nk65/keymaps/jtai/config.h
@@ -2,17 +2,14 @@
 #define IGNORE_MOD_TAP_INTERRUPT
 #define PERMISSIVE_HOLD
 
-// the default brightness
-#undef RGB_BACKLIGHT_BRIGHTNESS
-#define RGB_BACKLIGHT_BRIGHTNESS 77
-
 // the default effect (backlight off)
 #undef RGB_BACKLIGHT_EFFECT
 #define RGB_BACKLIGHT_EFFECT 0
 
  // the default color1 and color2
 #undef RGB_BACKLIGHT_COLOR_1
-#define RGB_BACKLIGHT_COLOR_1 { .h = 175, .s = 32 }
+#define RGB_BACKLIGHT_COLOR_1 { .h = 0, .s = 144 }
+
 #undef RGB_BACKLIGHT_COLOR_2
-#define RGB_BACKLIGHT_COLOR_2 { .h = 175, .s = 32 }
+#define RGB_BACKLIGHT_COLOR_2 { .h = 160, .s = 64 }
 
