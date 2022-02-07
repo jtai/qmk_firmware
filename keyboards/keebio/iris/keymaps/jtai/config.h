1553b0535cbbce438673b7e48f217c831275a9f7
Jon Tai <jon.tai@gmail.com>
2022-02-06 22:28:03 -0800

    Enable RGB effects
    Previously RGB was only enabled for functional indicators. This change enables a few built-in RGB effects, adds a new custom effect (underglow + reactive), and adds a new RGB control layer accessed with a combo (magic + L).

--- keyboards/keebio/iris/keymaps/jtai/config.h
+++ keyboards/keebio/iris/keymaps/jtai/config.h
@@ -25,5 +25,13 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define FORCED_SYNC_THROTTLE_MS_USER 100
 
 // configure combos
-#define COMBO_COUNT 5
+#define COMBO_COUNT 6
 #define COMBO_TERM_PER_COMBO
+
+// rgb matrix effects
+#define ENABLE_RGB_MATRIX_GRADIENT_UP_DOWN
+#define ENABLE_RGB_MATRIX_BREATHING
+#define ENABLE_RGB_MATRIX_CYCLE_PINWHEEL
+#define ENABLE_RGB_MATRIX_RAINDROPS
+#define RGB_MATRIX_KEYPRESSES
+#define ENABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
