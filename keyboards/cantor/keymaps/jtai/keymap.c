f774958619b40e52e37579f2fa0b929765779db2
Jon Tai <jon.tai@gmail.com>
2022-04-06 19:52:28 -0700

    Shift brackets one key left
    Like the parenthesis, these are hit with the fourth and fifth finger, so it makes sense to have them in the home position columns.
    
    Having the right shift key dedicated to a shift key also minimizes the chance that we accidentally type right bracket when we are trying to type a number key symbol. E.g., in the old position for right bracket, if we press MO(_LOWER) first, then right shift, then 2 (to make an @) we would instead get ]2.

--- keyboards/cantor/keymaps/jtai/keymap.c
+++ keyboards/cantor/keymaps/jtai/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_split_3x6_3(
         KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
         _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT,                         _______, _______, _______, _______, KC_MINS, KC_EQL,
-        _______, _______, _______, _______, _______, _______,                         _______, _______, _______, _______, KC_LBRC, KC_RBRC,
+        _______, _______, _______, _______, _______, _______,                         _______, _______, _______, KC_LBRC, KC_RBRC, _______,
                                             _______, _______, _______,       _______, _______, _______
     ),
 
