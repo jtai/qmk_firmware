48f132e5d0496e9181110e203b15eb5c6e1e51b1
Jon Tai <jon.tai@gmail.com>
2022-04-06 17:45:38 -0700

    Set indicator LEDs

--- keyboards/cantor/keymaps/jtai/config.h
+++ keyboards/cantor/keymaps/jtai/config.h
@@ -3,6 +3,11 @@
 // plug into right half
 #define MASTER_RIGHT
 
+// caps lock/caps word indicator
+#define LED_CAPS_LOCK_PIN C13
+#define LED_PIN_ON_STATE 0
+#define SPLIT_LED_STATE_ENABLE
+
 // configure grave esc
 #define GRAVE_ESC_ALT_OVERRIDE
 #define GRAVE_ESC_CTRL_OVERRIDE
