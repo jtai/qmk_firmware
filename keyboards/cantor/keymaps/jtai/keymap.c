f6b2c0f285f34a7d8e0c3fb7ed8080037a58e3a5
Jon Tai <jon.tai@gmail.com>
2022-04-27 22:06:51 -0700

    Change from toggle layer to set default layer
    Since the cantor lacks LEDs (that are not already in use for indicating other things), it's hard to know what keyboard layout is active -- especially on a password screen where typed characters are masked.
    
    Having dedicated buttons for each layout allows you to blindly hit the button for the layout to ensure you're in that layout.

--- keyboards/cantor/keymaps/jtai/keymap.c
+++ keyboards/cantor/keymaps/jtai/keymap.c
@@ -54,7 +54,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ADJUST] = LAYOUT_split_3x6_3(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_SPAM_BSPC,
         KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______,                         _______, _______, _______, _______, _______, _______,
-        _______, _______, _______, TG(_COLEMAK), _______, RESET,                      NK_TOGG, _______, _______, _______, _______, _______,
+        _______, _______, DF(_QWERTY), DF(_COLEMAK), _______, RESET,                  NK_TOGG, _______, _______, _______, _______, _______,
                                             _______, _______, _______,       _______, _______, _______
     )
 };
