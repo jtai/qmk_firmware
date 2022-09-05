fed838e84ddcc9c48c1fdfa96b75a2a13b8a9b44
Jon Tai <jon.tai@gmail.com>
2022-09-04 21:19:27 -0700

    Remove unused colemak layout

--- keyboards/cantor/keymaps/jtai/keymap.c
+++ keyboards/cantor/keymaps/jtai/keymap.c
@@ -1,10 +1,9 @@
 #include QMK_KEYBOARD_H
 
 #define _QWERTY 0
-#define _COLEMAK 1
-#define _LOWER 2
-#define _RAISE 3
-#define _ADJUST 4
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 3
 
 enum custom_keycodes {
     KC_SPAM_BSPC = SAFE_RANGE,
@@ -22,13 +21,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  LCTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, HYPR_T(KC_QUOT),
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                             KC_LALT, KC_LGUI, TD(TD_LWR), MO(_RAISE), KC_SPC,  KC_ENT
-    ),
-
-    [_COLEMAK] = LAYOUT_split_3x6_3(
-        _______, KC_Q,    KC_W,    KC_F,    KC_P,    KC_B,                            KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, _______,
-        _______, KC_A,    KC_R,    KC_S,    KC_T,    KC_G,                            KC_M,    KC_N,    KC_E,    KC_I,    KC_O,    _______,
-        _______, KC_Z,    KC_X,    KC_C,    KC_D,    KC_V,                            KC_K,    KC_H,    KC_COMM, KC_DOT,  KC_SLSH, _______,
-                                            _______, _______, _______,       _______, _______, _______
     ),
 
     // number/symbols and media layer, again try to keep symbols in similar positions (or at least relative positions) as a "normal" keyboard
@@ -53,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_ADJUST] = LAYOUT_split_3x6_3(
         _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_SPAM_BSPC,
         KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______,                         _______, _______, _______, _______, _______, LCTL(LALT(KC_DEL)),
-        _______, _______, DF(_QWERTY), DF(_COLEMAK), _______, QK_BOOT,                NK_TOGG, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, QK_BOOT,                         NK_TOGG, _______, _______, _______, _______, _______,
                                             _______, _______, _______,       _______, _______, _______
     )
 };
