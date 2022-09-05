7377556b3289e513c3dfdb7e4d4fbd6d9ec10dfd
Jon Tai <jon.tai@gmail.com>
2022-09-04 21:30:13 -0700

    Move function keys down to match number keys

--- keyboards/cantor/keymaps/jtai/keymap.c
+++ keyboards/cantor/keymaps/jtai/keymap.c
@@ -43,8 +43,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
     // function layer for F1..F12 keys and caps lock, as well as keyboard functions like reset and n-key rollover toggle
     [_ADJUST] = LAYOUT_split_3x6_3(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_SPAM_BSPC,
-        KC_CAPS, KC_F11,  KC_F12,  _______, _______, _______,                         _______, _______, _______, _______, _______, LCTL(LALT(KC_DEL)),
+        _______, KC_F11,  KC_F12,  _______, _______, _______,                         _______, _______, _______, _______, _______, KC_SPAM_BSPC,
+        KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  LCTL(LALT(KC_DEL)),
         _______, _______, _______, _______, _______, QK_BOOT,                         NK_TOGG, _______, _______, _______, _______, _______,
                                             _______, _______, _______,       _______, _______, _______
     )
