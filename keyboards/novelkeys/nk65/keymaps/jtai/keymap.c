7b1dc5b1324914c87d0d40850c635ff000453445
Jon Tai <jon.tai@gmail.com>
2022-09-12 11:55:03 -0700

    Rework keymap

--- keyboards/novelkeys/nk65/keymaps/jtai/keymap.c
+++ keyboards/novelkeys/nk65/keymaps/jtai/keymap.c
@@ -18,25 +18,18 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [0] = LAYOUT_65_ansi( /* Base */
-    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,     KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,\
-    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,     KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,\
-    MO(1),   KC_A,    KC_S,    KC_D,    KC_F,   KC_G,     KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,\
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,   KC_B,     KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,\
-    KC_LCTL, KC_LGUI, KC_LALT,                KC_SPC,                                KC_RALT, MO(2),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
+    KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,\
+    KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_END,\
+    KC_HYPR, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP,\
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN,\
+    KC_LCTL, KC_LGUI, KC_LALT,                   KC_SPC,                             KC_RALT, MO(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT),
 
-[1] = LAYOUT_65_ansi( /* Missing keys, Media keys, Extra arrow keys */
+[1] = LAYOUT_65_ansi( /* Missing keys, media keys, LED control, board functions */
     KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,\
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_UP,   _______, _______, _______, _______, _______, KC_PSCR,\
-    _______, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT, _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,          _______, KC_SLCK,\
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______, KC_PAUS,\
-    _______, _______, _______,                _______,                               _______, _______, _______, _______, _______, _______),
-
-[2] = LAYOUT_65_ansi( /* Caps lock, LED control, NKRO toggle, RESET */
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, EF_DEC,  EF_INC,  _______, _______,\
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, RESET,   _______,\
-    KC_CAPS, H1_DEC,  H1_INC,  S1_DEC,  S1_INC,  _______, _______, _______, _______, _______, _______, _______,          _______, _______,\
-    _______, H2_DEC,  H2_INC,  S2_DEC,  S2_INC,  _______, NK_TOGG, _______, _______, _______, _______, _______,          BR_INC,  _______,\
-    _______, _______, _______,                _______,                               _______, _______, _______, ES_DEC,  BR_DEC,  ES_INC),
+    _______, H1_DEC,  H1_INC,  S1_DEC,  S1_INC,  _______, _______, _______, _______, _______, _______, EF_DEC,  EF_INC,  RESET,   KC_PSCR,\
+    KC_CAPS, _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT, _______, _______, _______, _______, _______, _______,          _______, KC_SLCK,\
+    _______, H2_DEC,  H2_INC,  S2_DEC,  S2_INC,  _______, NK_TOGG, _______, _______, _______, _______, _______,          BR_INC,  KC_PAUS,\
+    _______, _______, _______,                   _______,                            _______, _______, _______, ES_DEC,  BR_DEC,  ES_INC),
 };
 
 enum combos {
