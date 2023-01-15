629ca4d5fa7476f77cb3c396ced05965a80ded4d
Jon Tai <jon.tai@gmail.com>
2023-01-14 16:48:18 -0800

    Update legacy keycodes

--- keyboards/cantor/keymaps/jtai/keymap.c
+++ keyboards/cantor/keymaps/jtai/keymap.c
@@ -17,7 +17,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // generally try to keep keys where they are on a "normal" keyboard (at least relatively, e.g., order of esc/tab/shift)
     // for modifiers, keep shift keys in normal position, but move main modifiers to thumb keys, and use mod tap for LCTL/HYPER modifiers
     [_QWERTY] = LAYOUT_split_3x6_3(
-        KC_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
+        QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
  LCTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, HYPR_T(KC_QUOT),
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
                                             KC_LALT, KC_LGUI, TD(TD_LWR), MO(_RAISE), KC_SPC,  KC_ENT
