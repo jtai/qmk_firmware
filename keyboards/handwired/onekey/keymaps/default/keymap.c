44fb3c79cd6f1eecf9dc62d7b9188c4d7ff4d7e5
Jon Tai <jon.tai@gmail.com>
2025-05-04 10:47:27 -0700

    Revert "Blackpill 1x1 minecraft miner"
    This reverts commit 67c27d3ca70e3f69cb845b9e4d725651576360bb.

--- keyboards/handwired/onekey/keymaps/default/keymap.c
+++ keyboards/handwired/onekey/keymaps/default/keymap.c
@@ -5,31 +5,3 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     LAYOUT_ortho_1x1(KC_A)
 };
-
-uint32_t deferred_callback(uint32_t trigger_time, void *cb_arg) {
-    tap_code(KC_LCTL);
-    return 1000;
-}
-
-bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-  static bool enable = false;
-  static deferred_token token;
-
-  switch (keycode) {
-    case KC_A:
-      if (record->event.pressed) {
-        enable = !enable;
-        if (enable) {
-          register_code(QK_MOUSE_BUTTON_1);
-          token = defer_exec(1000, deferred_callback, NULL);
-        } else {
-          unregister_code(QK_MOUSE_BUTTON_1);
-          cancel_deferred_exec(token);
-        }
-      }
-      return false; // Skip all further processing of this key
-    default:
-      return true; // Process all other keycodes normally
-  }
-}
-
