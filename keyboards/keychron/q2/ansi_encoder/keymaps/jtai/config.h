4819b88fae47f5fad3094819a829fe383c3e5f9d
Jon Tai <jon.tai@gmail.com>
2025-05-12 16:25:57 -0700

    LED indicators for mouse buttons

--- keyboards/keychron/q2/ansi_encoder/keymaps/jtai/config.h
+++ keyboards/keychron/q2/ansi_encoder/keymaps/jtai/config.h
@@ -1,6 +1,10 @@
 /* Caps word configuration */
 #define BOTH_SHIFTS_TURNS_ON_CAPS_WORD
 
-/* Enable caps word LED (both Shift keys) */
+/* LEDs for caps word (both Shift keys) */
 #define CAPS_WORD_LED_INDEX_1 44
 #define CAPS_WORD_LED_INDEX_2 55
+
+/* LEDs for mouse buttons */
+#define MOUSE_BUTTON_LED_INDEX_1 26
+#define MOUSE_BUTTON_LED_INDEX_2 27
