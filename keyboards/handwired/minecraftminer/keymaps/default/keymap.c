545a31379e5d9132d4ca740d07de801ee149b117
Jon Tai <jon.tai@gmail.com>
2025-06-02 01:49:58 -0700

    Rewrite tap mode
    * Consolidate all state into one struct
    * Don't duplicate unregister_code() call when tapping
    * Avoid using tap_code_delay() since it blocks the main loop, manipulate the indicator state in a separate deferred callback
    * Move actual LED GPIO manipulation to a housekeeping task to make this more similar to other keyboards

--- keyboards/handwired/minecraftminer/keymaps/default/keymap.c
+++ keyboards/handwired/minecraftminer/keymaps/default/keymap.c
@@ -6,28 +6,31 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 
-typedef struct mouse_key_deferred_data {
-  uint16_t keycode;
-  uint32_t led;
-  uint32_t delay;
-} mouse_key_deferred_data;
-
 typedef struct mouse_key_state {
   bool toggled;
-  mouse_key_deferred_data data;
+  bool hold;
+  uint16_t keycode;
+  uint32_t delay;
   deferred_token token;
+  bool indicator;
 } mouse_key_state;
 
+static mouse_key_state mouse_key_states[2];
+
+uint32_t mouse_key_indicator_deferred_callback(uint32_t trigger_time, void *cb_arg) {
+  mouse_key_state* state = (mouse_key_state*)cb_arg;
+  state->indicator = false;
+  return 0;
+}
+
 uint32_t mouse_key_deferred_callback(uint32_t trigger_time, void *cb_arg) {
-    mouse_key_deferred_data* data = (mouse_key_deferred_data*)cb_arg;
-    if (data->led) {
-      gpio_write_pin_high(data->led);
-    }
-    tap_code_delay(data->keycode, 50);
-    if (data->led) {
-      gpio_write_pin_low(data->led);
-    }
-    return data->delay;
+  mouse_key_state* state = (mouse_key_state*)cb_arg;
+  if (!state->hold) {
+    state->indicator = true;
+    defer_exec(25, mouse_key_indicator_deferred_callback, state);
+  }
+  tap_code(state->keycode);
+  return state->delay;
 }
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
@@ -35,28 +38,34 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     case LT(0, MS_BTN1):
     case LT(0, MS_BTN2):
       if (record->event.pressed) {
-        static mouse_key_state mouse_key_states[2];
-
         unsigned char index = keycode - LT(0, MS_BTN1); // Assumes keycodes are adjacent
         mouse_key_state* state = &mouse_key_states[index];
+
         uint16_t tap_keycode = QK_LAYER_TAP_GET_TAP_KEYCODE(keycode);
-        uint32_t led = index == 0 ? LED1 : LED2;
 
         state->toggled = !state->toggled;
         if (state->toggled) {
           if (record->tap.count) {
-            state->data = (mouse_key_deferred_data) {tap_keycode, led, 1000};
-            state->token = defer_exec(1, mouse_key_deferred_callback, &state->data);
+            state->hold = false;
+            state->keycode = tap_keycode;
+            state->delay = 1000;
+            state->token = defer_exec(1, mouse_key_deferred_callback, state);
           } else {
+            state->hold = true;
+            state->keycode = KC_LCTL;
+            state->delay = 60000;
+            state->token = defer_exec(60000, mouse_key_deferred_callback, state);
+
             register_code(tap_keycode);
-            gpio_write_pin_high(led);
-            state->data = (mouse_key_deferred_data) {KC_LCTL, 0, 60000};
-            state->token = defer_exec(60000, mouse_key_deferred_callback, &state->data);
+            state->indicator = true;
           }
         } else {
-          unregister_code(tap_keycode);
-          gpio_write_pin_low(led);
           cancel_deferred_exec(state->token);
+
+          if (state->hold) {
+            unregister_code(tap_keycode);
+            state->indicator = false;
+          }
         }
       }
       return false; // Skip all further processing of this key
@@ -65,4 +74,9 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     default:
       return true; // Process all other keycodes normally
   }
+}
+
+void housekeeping_task_user(void) {
+  gpio_write_pin(LED1, mouse_key_states[0].indicator);
+  gpio_write_pin(LED2, mouse_key_states[1].indicator);
 }
