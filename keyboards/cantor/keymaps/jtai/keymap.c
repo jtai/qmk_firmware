00c62efbabd0c00d45162e006126ec56c4f4db09
Jon Tai <jon.tai@gmail.com>
2026-04-16 19:09:45 -0700

    Shift [] and \ one key over to the left
    This lines up [] with <> which is more logical.

--- keyboards/cantor/keymaps/jtai/keymap.c
+++ keyboards/cantor/keymaps/jtai/keymap.c
@@ -29,7 +29,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_LOWER] = LAYOUT_split_3x6_3(
         KC_GRV,  _______, KC_VOLD, KC_VOLU, KC_MPLY, KC_MNXT,                         _______, _______, _______, KC_MINS, KC_EQL,  _______,
         _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                            KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
-        _______, _______, _______, _______, _______, _______,                         _______, _______, _______, KC_LBRC, KC_RBRC, RSFT_T(KC_BSLS),
+        _______, _______, _______, _______, _______, _______,                         _______, _______, KC_LBRC, KC_RBRC, KC_BSLS, _______,
                                             _______, _______, _______,       _______, _______, _______
     ),
 
