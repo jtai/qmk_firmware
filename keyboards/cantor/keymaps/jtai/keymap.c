7cea5256aeceeccdf24611f631de04053c3fba66
Jon Tai <jon.tai@gmail.com>
2026-04-16 19:06:02 -0700

    Allow swapping LALT and LGUI
    Also remove QK_BOOT key and split NK_TOGG into NK_ON and NK_OFF.

--- keyboards/cantor/keymaps/jtai/keymap.c
+++ keyboards/cantor/keymaps/jtai/keymap.c
@@ -20,7 +20,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         QK_GESC, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                            KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
  LCTL_T(KC_TAB), KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                            KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, HYPR_T(KC_QUOT),
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                            KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                                            KC_LALT, KC_LGUI, TD(TD_LWR), MO(_RAISE), KC_SPC,  KC_ENT
+                                            KC_LGUI, KC_LALT, TD(TD_LWR), MO(_RAISE), KC_SPC,  KC_ENT
     ),
 
     // number/symbols and media layer, again try to keep symbols in similar positions (or at least relative positions) as a "normal" keyboard
@@ -41,11 +41,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                             _______, _______, _______,       _______, _______, _______
     ),
 
-    // function layer for F1..F12 keys and caps lock, as well as keyboard functions like reset and n-key rollover toggle
+    // function layer for F1..F12 keys and caps lock, as well as keyboard functions like Windows/Mac mode and n-key rollover mode
     [_ADJUST] = LAYOUT_split_3x6_3(
         _______, KC_F11,  KC_F12,  _______, _______, _______,                         _______, _______, _______, _______, _______, KC_SPAM_BSPC,
         KC_CAPS, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                           KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  LCTL(LALT(KC_DEL)),
-        _______, _______, _______, _______, _______, QK_BOOT,                         NK_TOGG, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, AG_LNRM, AG_LSWP,                         NK_OFF,  NK_ON,   _______, _______, _______, _______,
                                             _______, _______, _______,       _______, _______, _______
     )
 };
@@ -109,13 +109,23 @@ void dance_lower_finished(tap_dance_state_t *state, void *user_data) {
         case 1:
             break;
         case 2:
-            dance_lower_gui_hold = true;
-            register_code(KC_LGUI);
+            if (keymap_config.swap_lalt_lgui) {
+                dance_lower_gui_hold = true;
+                register_code(KC_LGUI);
+            } else {
+                dance_lower_alt_hold = true;
+                register_code(KC_LALT);
+            }
             caps_word_off();
             break;
         case 3:
-            dance_lower_alt_hold = true;
-            register_code(KC_LALT);
+            if (keymap_config.swap_lalt_lgui) {
+                dance_lower_alt_hold = true;
+                register_code(KC_LALT);
+            } else {
+                dance_lower_gui_hold = true;
+                register_code(KC_LGUI);
+            }
             caps_word_off();
             break;
         default:
